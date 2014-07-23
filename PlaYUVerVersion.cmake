ee4f969a5a63553574db9c26f78f4305cf129c6c
Joao Carreira <jfmcarreira@gmail.com>
2014-07-23 19:23:29 +0100

    Set version number from the git tag and number of commits

--- PlaYUVerVersion.cmake
+++ PlaYUVerVersion.cmake
@@ -1,4 +1,24 @@
-SET( PLAYUVER_VERSION_MAJOR "0" )
-SET( PLAYUVER_VERSION_MINOR "0" )
-SET( PLAYUVER_VERSION_PATCH  "9999" )
+
+include(cmake/GetGitRevisionDescription.cmake)
+
+git_describe(__version --tags)
+
+string(REPLACE "-" ";" __version_list ${__version})
+list(GET __version_list 0 __version)
+list(GET __version_list 1 __patch)
+string(REPLACE "." ";" __version_list ${__version})
+list(GET __version_list 0 __major)
+list(GET __version_list 1 __minor)
+
+
+SET( PLAYUVER_VERSION_MAJOR ${__major})
+SET( PLAYUVER_VERSION_MINOR ${__minor})
+SET( PLAYUVER_VERSION_PATCH ${__patch})
 SET( PLAYUVER_VERSION "${PLAYUVER_VERSION_MAJOR}.${PLAYUVER_VERSION_MINOR}.${PLAYUVER_VERSION_PATCH}")
+
+unset(__version_list) 
+unset(__version)
+unset(__patch)
+unset(__major)
+unset(__minor)
+

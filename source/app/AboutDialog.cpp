784e009c873f8759dbda599190537005cfd9461f
Joao Carreira <jfmcarreira@gmail.com>
2014-10-04 14:55:20 +0100

    Improve about dialog

--- source/app/AboutDialog.cpp
+++ source/app/AboutDialog.cpp
@@ -35,40 +35,43 @@
 AboutDialog::AboutDialog(QWidget *parent)
     : QDialog(parent)
 {
+  QPixmap logo = QPixmap(":/images/playuver-backgroud-logo.png").scaled( QSize( 350,350), Qt::KeepAspectRatio );
 	QLabel *pixmapLabel = new QLabel;
-	//pixmapLabel->setPixmap(QPixmap(":/icons/qtikz-128.png"));
-	QLabel *label = new QLabel(QString("<h1>%1 %2</h1><p>%3</p><p>%4</p>")
+	pixmapLabel->setPixmap( logo );
+
+
+	QLabel *label = new QLabel(QString("<h1>%1 Version %2</h1><p>%3</p><p>%4</p>")
 	    .arg(QApplication::applicationName())
 	    .arg(QApplication::applicationVersion())
-	    .arg(tr("Copyright 2014 Luis Lucas and Joao Carreira"))
-	    .arg(tr("plaYUVer is an open-source QT based raw video player.")));
+	    .arg(tr("Enhanced open-source QT based raw video player"))
+	    .arg(tr("Copyright 2014 Luis Lucas and Joao Carreira")));
 	label->setWordWrap(true);
 
 	QWidget *topWidget = new QWidget;
-	QHBoxLayout *topLayout = new QHBoxLayout;
+	QVBoxLayout *topLayout = new QVBoxLayout;
 	topLayout->addWidget(pixmapLabel);
 	topLayout->addWidget(label);
 	topWidget->setLayout(topLayout);
 
-	QTextEdit *textEdit = new QTextEdit(tr("<p>This program is free "
-	    "software; you can redistribute it and/or modify it under the "
-	    "terms of the GNU General Public License as published by the "
-	    "Free Software Foundation; either version 2 of the License, "
-	    "or (at your option) any later version.</p>"
-	    "<p>This program is distributed in the hope that it will "
-	    "be useful, but WITHOUT ANY WARRANTY; without even the implied "
-	    "warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  "
-	    "See the GNU General Public License for more details.</p>"));
-	textEdit->setReadOnly(true);
+//	QTextEdit *textEdit = new QTextEdit(tr("<p>This program is free "
+//	    "software; you can redistribute it and/or modify it under the "
+//	    "terms of the GNU General Public License as published by the "
+//	    "Free Software Foundation; either version 2 of the License, "
+//	    "or (at your option) any later version.</p>"
+//	    "<p>This program is distributed in the hope that it will "
+//	    "be useful, but WITHOUT ANY WARRANTY; without even the implied "
+//	    "warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  "
+//	    "See the GNU General Public License for more details.</p>"));
+//	textEdit->setReadOnly(true);
 	QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok);
 	connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
 
 	QVBoxLayout *mainLayout = new QVBoxLayout(this);
 	mainLayout->addWidget(topWidget);
-	mainLayout->addWidget(textEdit);
+	//mainLayout->addWidget(textEdit);
 	mainLayout->addWidget(buttonBox);
 	mainLayout->setSpacing(10);
 	buttonBox->setFocus();
 
-	setWindowTitle(tr("About %1").arg("playuver"));
+	setWindowTitle(tr("About %1").arg(QApplication::applicationName()));
 }

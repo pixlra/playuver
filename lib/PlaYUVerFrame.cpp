594b0eee1d90493ae87f741f6250668bf231ff7f
Joao Carreira <jfmcarreira@gmail.com>
2016-11-25 09:53:05 +0000

    Bugfix PSNR calculation

--- lib/PlaYUVerFrame.cpp
+++ lib/PlaYUVerFrame.cpp
@@ -1327,7 +1327,7 @@ Double PlaYUVerFrame::getMSE( PlaYUVerFrame* Org, Int component )
 
 Double PlaYUVerFrame::getPSNR( PlaYUVerFrame* Org, Int component )
 {
-  UInt uiMaxValue = ( 1 << Org->getBitsPel() );
+  UInt uiMaxValue = ( 1 << Org->getBitsPel() - 1 );
   Double uiMaxValueSquare = uiMaxValue * uiMaxValue;
   Double dPSNR = 100;
   Double dMSE = getMSE( Org, component );
